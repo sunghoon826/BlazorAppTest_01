b787ecb597a243b819a80f60f0ac0342ad6dff72
sunghoon826 <sunghoon898@gmail.com>
2023-10-17 17:27:52 +0900

    no message

--- Buffet/BlazorAppTest_01/Data/ApplicationDbContext.cs
+++ Buffet/BlazorAppTest_01/Data/ApplicationDbContext.cs
@@ -16,5 +16,25 @@ namespace BlazorAppTest_01.Data
         public DbSet<Noodle> Noodles { get; set; }
 
         public DbSet<Garnish> Garnishes { get; set; }
+
+        /// <summary>
+        /// 모델(테이블)이 생성될 때 처음 실행 
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Broth>().HasData(
+                new Broth() { Id = 1, Name = "콩국물", IsVegan = true },
+                new Broth() { Id = 2, Name = "멸치국물", IsVegan = false }              
+                );
+
+            builder.Entity<Noodle>().HasData(
+                new Noodle { Id = 1, Name = "콩국수", BrothId = 1 },
+                new Noodle { Id = 2, Name = "잔치국수", BrothId = 2 }             
+                
+               );
+               
+        }
     }
 }
---
+++ Buffet/BlazorAppTest_01/Data/Migrations/20231017070520_SeedData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorAppTest_01.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Broths",
+                columns: new[] { "Id", "IsVegan", "Name" },
+                values: new object[,]
+                {
+                    { 1, true, "콩국물" },
+                    { 2, false, "멸치국물" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Noodles",
+                columns: new[] { "Id", "BrothId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "콩국수" },
+                    { 2, 2, "잔치국수" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Noodles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Noodles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Broths",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Broths",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
