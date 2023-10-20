682b642e8560feaf5ba5bf02a3898a50fe488f2e
sunghoon826 <sunghoon898@gmail.com>
2023-10-20 17:36:46 +0900

    70강

--- Buffet/BlazorAppTest_01/Migrations/20231020073629_CandidatesNames.Designer.cs
+++ Buffet/BlazorAppTest_01/Migrations/20231020073629_CandidatesNames.Designer.cs
@@ -4,6 +4,7 @@ using BlazorAppTest_01.Models.Candidates;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorAppTest_01.Migrations
 {
     [DbContext(typeof(CandidateAppDbContext))]
-    partial class CandidateAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231020073629_CandidatesNames")]
+    partial class CandidatesNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
