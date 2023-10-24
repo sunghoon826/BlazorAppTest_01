a4eb4821dca3fcd7849eae61b1e321f088e00c1d
sunghoon826 <sunghoon898@gmail.com>
2023-10-24 10:09:50 +0900

    Names

---
+++ Buffet/BlazorAppTest_01/Areas/Identity/Pages/Account/Manage/CandidatesNames.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorAppTest_01.Areas.Identity.Pages.Account.Manage
+{
+    public class CandidatesNamesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Buffet/BlazorAppTest_01/Program.cs
+++ Buffet/BlazorAppTest_01/Program.cs
@@ -1,13 +1,12 @@
 using BlazorAppTest_01.Areas.Identity;
 using BlazorAppTest_01.Areas.Identity.Models;
-using BlazorAppTest_01.Areas.Identity.Services;
 using BlazorAppTest_01.Data;
 using BlazorAppTest_01.Models.Candidates;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
-
+using BlazorAppTest_01.Areas.Identity.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,39 +16,48 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 
-//���ο� DbContext �߰�
-//builder.Services.AddDbContext<CandidateAppDbContext>(options =>
-//    options.UseSqlServer(connectionString));
-builder.Services.AddDbContextFactory<CandidateAppDbContext>(options =>
-    options.UseSqlServer(connectionString)); 
+#region 새로운 DbContext 추가 - CandidateAppDbContext
+// 새로운 DbContext 추가 
+
+//[a] MVC, RazorPages, Web API에서는 DbContext 사용 가능
+//builder.Services.AddDbContext<CandidateAppDbContext>(options => options.UseSqlServer(connectionString));
+
+//[b] Blazor Server에서는 DbContextFactory 사용 권장
+builder.Services.AddDbContextFactory<CandidateAppDbContext>(options => options.UseSqlServer(connectionString));
+#endregion
+
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
 //builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
 //    .AddEntityFrameworkStores<ApplicationDbContext>();
-
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor()
+    .AddCircuitOptions(options => { options.DetailedErrors = true; });
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-// ���Ӽ� ���� �߰�
+
+// 종속성 주입 추가 
 builder.Services.AddTransient<IEmailSender, EmailSender>();
+
 
 var app = builder.Build();
 
-// ���� ȯ�濡�� Update-Database, Seed ������ �߰�
+// 개발 환경에서 Update-Database, Seed 데이터 추가
 if (app.Environment.IsDevelopment())
 {
     await CheckCandidateDbMigrated(app.Services);
     CandidateSeedData(app);
     CandidateDbInitializer.Initialize(app);
 }
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -69,6 +77,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -80,24 +89,29 @@ app.MapFallbackToPage("/_Host");
 
 app.Run();
 
-#region CandidateSeedData: Candidates ���̺��� �⺻ ������ �Է�
-// Candidates ���̺��� �⺻ ������ �Է�
+
+
+
+
+#region CandidateSeedData: Candidates 테이블에 기본 데이터 입력
+// Candidates 테이블에 기본 데이터 입력
 static void CandidateSeedData(WebApplication app)
 {
-    // https://docs.microsoft.com/ko-kr/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-6.0#resolve-a-service-at-app-start-up
+    // https://docs.microsoft.com/ko-kr/aspnet/core/fundamentals/dependency-injection
+    // ?view=aspnetcore-6.0#resolve-a-service-at-app-start-up
     using (var serviceScope = app.Services.CreateScope())
     {
         var services = serviceScope.ServiceProvider;
 
         var candidateDbContext = services.GetRequiredService<CandidateAppDbContext>();
 
-        // Candidates ���̺��� �����Ͱ� ���� ������ ������ �Է�
+        // Candidates 테이블에 데이터가 없을 때에만 데이터 입력
         if (!candidateDbContext.Candidates.Any())
         {
             candidateDbContext.Candidates.Add(
-                new Candidate { FirstName = "�浿", LastName = "ȫ", IsEnrollment = false });
+                new Candidate { FirstName = "길동", LastName = "홍", IsEnrollment = false });
             candidateDbContext.Candidates.Add(
-                new Candidate { FirstName = "�λ�", LastName = "��", IsEnrollment = false });
+                new Candidate { FirstName = "두산", LastName = "백", IsEnrollment = false });
 
             candidateDbContext.SaveChanges();
         }
@@ -105,16 +119,15 @@ static void CandidateSeedData(WebApplication app)
 }
 #endregion
 
-#region CheckCandidateDbMigrated: �����ͺ��̽� ���̱׷��̼� ����
-// �����ͺ��̽� ���̱׷��̼� ����
+#region CheckCandidateDbMigrated: 데이터베이스 마이그레이션 진행
+// 데이터베이스 마이그레이션 진행
 async Task CheckCandidateDbMigrated(IServiceProvider services)
 {
     using var scope = services.CreateScope();
-
     using var candidateContext = scope.ServiceProvider.GetService<CandidateAppDbContext>();
     if (candidateContext is not null)
     {
         await candidateContext.Database.MigrateAsync();
     }
-} 
+}
 #endregion
